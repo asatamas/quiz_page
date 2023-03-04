2bf2b872fa18ac5fef014d41f3d9a21c27f562cd
Matas <matas.asaciovas2001@gmail.com>
2023-03-04 23:37:49 +0200

    Initial commit. Created "Pages" and "Components" folders. Included Bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutPage from './Pages/AboutPage';
+
+function App() {
+  return (
+    <>
+    <AboutPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+export default function AboutPage() {
+  return (
+    <>
+    <div>Helo</div>
+    <Button variant = "primary">yes</Button>
+    </>
+  )
+}
