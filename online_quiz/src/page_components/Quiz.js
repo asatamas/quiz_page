51de9aa074fe74a37c0e56e0d22d200472f1dd02
Tomazauras <97549370+Tomazauras@users.noreply.github.com>
2023-03-26 21:04:42 +0300

    Removed image usings

--- online_quiz/src/page_components/Quiz.js
+++ online_quiz/src/page_components/Quiz.js
@@ -1,44 +1,33 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../css/QuizPage.css";
 import "../css/HomePage.css";
-import quizPic1 from "../assets/quizPage-1.JPG";
-import quizPic2 from "../assets/quizPage-2.JPG";
-import quizPic3 from "../assets/quizPage-3.JPG";
 import defaultImg from "../assets/default.jpg";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
-const Quiz = ({quiz}) => {
-
-    return (
-        
-      <>
-        
-        <div className="QPageQuizContainer">
-          <div className="QPageQuizInfoContainer">
-            <p1 className="QPageQuizHeader">
-              {quiz.name}
-              <br></br>
-              <p2 className="QPageQuizText">
-                {quiz.description}
-              </p2>
-            </p1>
-            <img
-              className="QPageQuizImage QPageQuizImageBorder"
-              src={quiz.image_url ? quiz.image_url : defaultImg}
-            />
-          </div>
-          <div className="QPageQuizButtonContainer">
-            <Link to="/QuizPage/Quiz1">
-              <button className="quiz-btn">Solve quiz</button>
-            </Link>
-          </div>
-          {/* define the route for quiz1 */}
+const Quiz = ({ quiz }) => {
+  return (
+    <>
+      <div className="QPageQuizContainer">
+        <div className="QPageQuizInfoContainer">
+          <p1 className="QPageQuizHeader">
+            {quiz.name}
+            <br></br>
+            <p2 className="QPageQuizText">{quiz.description}</p2>
+          </p1>
+          <img
+            className="QPageQuizImage QPageQuizImageBorder"
+            src={quiz.image_url ? quiz.image_url : defaultImg}
+          />
         </div>
-        
-      </>
-    );
-
+        <div className="QPageQuizButtonContainer">
+          <Link to="/QuizPage/Quiz1">
+            <button className="quiz-btn">Solve quiz</button>
+          </Link>
+        </div>
+        {/* define the route for quiz1 */}
+      </div>
+    </>
+  );
 };
 
 export default Quiz;
