e80d6015b8c399c4f99d9149681f9df175bfc0e8
Jurgis Andziulis <jurand1@ktu.lt>
2023-03-26 19:46:44 +0300

    Styled the navbar and added a logo to it

--- online_quiz/src/page_components/NavBar.js
+++ online_quiz/src/page_components/NavBar.js
@@ -5,14 +5,19 @@ export default function NavBar() {
   return (
     <nav className="nav">
       <Link to="/HomePage" className="online_quiz">
-        QuizHub
+        <img
+          src="https://cdn.discordapp.com/attachments/1019698765969490052/1089578819704328296/image-removebg-preview.png"
+          alt="QuizHub"
+        />
       </Link>
+
       <ul>
         <CustomLink to="/HomePage">Home</CustomLink>
         <CustomLink to="/AboutPage">About Us</CustomLink>
         <CustomLink to="/HowToPage">How To</CustomLink>
         <CustomLink to="/QuizPage">Quizzes</CustomLink>
         <CustomLink to="/Question">Question</CustomLink>
+        <div class="dot"></div>
       </ul>
     </nav>
   );
